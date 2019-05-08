e10ff170c948a702a2c68edec4eae76a24004476
MFredX <sachindaham@gmail.com>
2019-05-08 18:56:51 +1000

    Figuring out git mate

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Buttons from "./buttons.js";
 import {FormComp} from "./form.js";
 // import {SearchApp} from "./search.js";
 import './styles.css';
-
+//Figuring git out mate
 function App() {
   return (
     <div className="First">
